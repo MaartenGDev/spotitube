47afb4bec27a7c2754e95444695739646db6bdcd
MaartenGDev <maarten.grootoonk@gmail.com>
2018-10-13 00:34:56 +0200

    Calculate playlist length based on total of track durations

--- src/main/java/me/maartendev/spotitube/dto/PlayListCollectionDTO.java
+++ src/main/java/me/maartendev/spotitube/dto/PlayListCollectionDTO.java
@@ -16,6 +16,14 @@ public class PlayListCollectionDTO {
     }
 
     public int getLength() {
-        return length;
+        int totalTrackLength = 0;
+
+        for(PlayListDTO playListDTO : this.getPlaylists()){
+            for(TrackDTO trackDTO : playListDTO.getTracks()){
+                totalTrackLength += trackDTO.getDuration();
+            }
+        }
+
+        return totalTrackLength;
     }
 }
