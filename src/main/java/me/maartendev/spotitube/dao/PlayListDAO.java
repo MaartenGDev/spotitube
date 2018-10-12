7f92d13da9ddf4398acedc586fd989466b230e6c
MaartenGDev <maarten.grootoonk@gmail.com>
2018-10-13 00:13:39 +0200

    Update owner usage

--- src/main/java/me/maartendev/spotitube/dao/PlayListDAO.java
+++ src/main/java/me/maartendev/spotitube/dao/PlayListDAO.java
@@ -25,10 +25,10 @@ public class PlayListDAO extends DAO<PlayListDTO> {
 
     public PlayListDTO create(PlayListDTO playList) {
         Map<Integer, Object> bindings = new HashMap<>();
-        bindings.put(1, playList.getName());
-        bindings.put(2, true);
+        bindings.put(1, 1);
+        bindings.put(2, playList.getName());
 
-        this.runQuery("INSERT INTO playlists(name, owner) VALUES(?,?)", bindings);
+        this.runQuery("INSERT INTO playlists(owner_id,name) VALUES(?, ?)", bindings);
 
         return playList;
     }
