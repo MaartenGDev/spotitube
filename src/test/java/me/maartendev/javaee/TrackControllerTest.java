27a94267d844593591afe62ea685e7768e5678fd
MaartenGDev <maarten.grootoonk@gmail.com>
2018-10-12 10:00:52 +0200

    Add tests for Playlist and Track controller

---
+++ src/test/java/me/maartendev/javaee/TrackControllerTest.java
@@ -0,0 +1,52 @@
+package me.maartendev.javaee;
+
+import me.maartendev.javaee.dao.TrackDAO;
+import me.maartendev.javaee.dto.TrackCollectionDTO;
+import me.maartendev.javaee.dto.TrackDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrackControllerTest {
+    @Test
+    public void testShouldReturnAllTracksIfNoPlayListIdWasSpecified() {
+        TrackController trackController = new TrackController();
+
+        TrackDAO trackDAO = Mockito.mock(TrackDAO.class);
+        trackController.setTrackDAO(trackDAO);
+
+        List<TrackDTO> tracks = new ArrayList<>();
+        tracks.add(new TrackDTO());
+
+        TrackCollectionDTO trackCollectionToReturn = new TrackCollectionDTO(tracks);
+
+        Mockito.when(trackDAO.all()).thenReturn(trackCollectionToReturn);
+
+        TrackCollectionDTO returnedCollection = (TrackCollectionDTO) trackController.index(0).getEntity();
+
+        Assertions.assertEquals(returnedCollection.getTracks().size(), trackCollectionToReturn.getTracks().size());
+    }
+
+
+    @Test
+    public void testShouldReturnAllTracksOfAPlayListIfAnIdWasSpecified() {
+        TrackController trackController = new TrackController();
+
+        TrackDAO trackDAO = Mockito.mock(TrackDAO.class);
+        trackController.setTrackDAO(trackDAO);
+
+        List<TrackDTO> tracks = new ArrayList<>();
+        tracks.add(new TrackDTO());
+
+        TrackCollectionDTO trackCollectionToReturn = new TrackCollectionDTO(tracks);
+
+        Mockito.when(trackDAO.allNotInPlaylistId(3)).thenReturn(trackCollectionToReturn);
+
+        TrackCollectionDTO returnedCollection = (TrackCollectionDTO) trackController.index(3).getEntity();
+
+        Assertions.assertEquals(returnedCollection.getTracks().size(), trackCollectionToReturn.getTracks().size());
+    }
+}
